731b798fbc279e038ae306aca59807c47c70cf70
zzr2002314 <153415041+zzr2002314@users.noreply.github.com>
2024-05-31 12:37:13 +0800

    Update collect.py

--- aggregate/subscribe/collect.py
+++ aggregate/subscribe/collect.py
@@ -224,7 +224,7 @@ if __name__ == "__main__":
         "--filename",
         type=str,
         required=False,
-        default="proxies.yaml",
+        default="ProxyPool.yaml",
         help="proxies filename",
     )
 
